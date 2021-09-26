f46788988f3a085f2041d1a722b457281296a6a4
Blaine Motsinger <blaine@renderorange.com>
2021-09-25 20:28:42 -0500

    Add GetTimers and AddTimer methods

--- lib/RT/Extension/PersistentTimers.pm
+++ lib/RT/Extension/PersistentTimers.pm
@@ -43,6 +43,120 @@ Add this line:
 
 =back
 
+=head1 METHODS
+
+=head2 GetTimers
+
+Get timer attribute objects for a given user id.
+
+ my ( $ret, $msg ) = RT::Extension::PersistentTimers->GetTimers( user_id => $user_id );
+
+=head3 ARGUMENTS
+
+=over
+
+=item user_id (required)
+
+The user id to get timer attribute objects for.
+
+=back
+
+=head3 RETURNS
+
+On success, an arrayref of C<RT::Attribute> objects.
+
+On failure, a list of 2 members; C<undef> and the failure message.
+
+=cut
+
+sub GetTimers {
+    my $self = shift;
+    my $arg  = {
+        user_id => undef,
+        @_,
+    };
+
+    unless ( defined $arg->{user_id} ) {
+        return ( 0, 'Argument user_id is required' );
+    }
+
+    require RT::User;
+    my $user_obj = RT::User->new( RT->SystemUser );
+    $user_obj->Load( $arg->{user_id} );
+
+    my @timers = ( $user_obj->Attributes->Named( 'Timer' ) );
+    return \@timers;
+}
+
+=head2 AddTimer
+
+Add a timer attribute object for a given user id.
+
+ my ( $ret, $msg ) = RT::Extension::PersistentTimers->AddTimer( user_id => $user_id, ticket_id => $ticket_id );
+
+=head3 ARGUMENTS
+
+=over
+
+=item user_id (required)
+
+The user id to add the timer attribute for.
+
+=item ticket_id (required)
+
+The ticket id to add the timer for.
+
+=back
+
+=head3 RETURNS
+
+On success, the id of the new timer attribute.
+
+On failure, a list of 2 members; C<undef> and the failure message.
+
+=cut
+
+sub AddTimer {
+    my $self = shift;
+    my $arg  = {
+        user_id   => undef,
+        ticket_id => undef,
+        @_,
+    };
+
+    foreach my $required ( keys %{$arg} ) {
+        unless ( defined $arg->{$required} ) {
+            return ( 0, "Argument $required is required" );
+        }
+    }
+
+    require RT::User;
+    my $user_obj = RT::User->new( RT->SystemUser );
+    $user_obj->Load( $arg->{user_id} );
+
+    foreach my $timer_obj ( @{ $self->GetTimers( user_id => $arg->{user_id} ) } ) {
+        my $content = $timer_obj->Content();
+        if ( $content->{ticket}{id} == $arg->{ticket_id} ) {
+            return ( 0, 'Timer for ticket ' . $arg->{ticket_id} . ' already exists' );
+        }
+    }
+
+    require RT::Attribute;
+    my $attr = RT::Attribute->new( $user_obj );
+
+    return $attr->Create(
+        Name        => 'Timer',
+        Description => 'Ticket timer',
+        Content     => {
+            ticket  => {
+                id => $arg->{ticket_id},
+            },
+            seconds => '0',
+        },
+        Object => $user_obj,
+    );
+}
+
 =head1 AUTHOR
 
 Blaine Motsinger <blaine@renderorange.com>
